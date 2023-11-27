1680057405b67b35390bb9d81a604b507d819811
mlhoutel <mlhoutel@gmail.com>
2023-11-27 13:20:29 +0100

    build: :zap: Fix deployment workflow for demo website wasm build

--- docs/static/compy_wasm_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function transform_nothing(a: number, b: number, c: number): void;
-export function transform_oneline(a: number, b: number, c: number): void;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
-export function __wbindgen_free(a: number, b: number): void;
